b91de9a718248bae5521f16f5ff20dadea2b4556
Habin Bahith <88604836+HabinBahith@users.noreply.github.com>
2026-06-28 10:51:43 +0530

    done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+    const reversedstring = cleanedString.split('').reverse().join('');
+
+    return reversedstring === cleanedString;
 };
 
 // Do not edit below this line
